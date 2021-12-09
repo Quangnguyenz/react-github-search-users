2c6ce9a80915d3ea96bc0169be7755a193673f6c
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-12-09 10:59:40 -0600

    worked on a few components and context
    worked on a few component, set up a global context

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,32 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const { avatar_url, html_url, name, company, blog, bio, location, twitter_username, } = githubUser
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name}></img>
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'not found'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className='bio'>{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness></MdBusiness> {company || 'not listed'}
+        </p>
+        <p>
+          <MdLocationOn></MdLocationOn> {location || 'not listed'}
+        </p>
+        <a href={`https://${blog}`}>
+          <MdLink></MdLink> {blog || 'not listed'}
+        </a>
+      </div>
+    </Wrapper >
+  )
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,21 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+//acces to provider and consumer - Github.Provider
+
+const GithubProvider = ({ children }) => {
+    const [githubUser, setGithubUser] = useState(mockUser);
+    const [repos, setrepos] = useState(mockRepos);
+    const [followers, setFollwers] = useState(mockFollowers);
+    return <GithubContext.Provider
+        value={{
+            githubUser,
+            repos,
+            followers,
+        }}>{children}</GithubContext.Provider>
+}
+
+export { GithubContext, GithubProvider }
