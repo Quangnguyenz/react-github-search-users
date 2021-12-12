a92d3cadebdc7f97447baed3a67a01769cc5c4c1
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-12-11 18:08:29 -0600

    Adding user authentication functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,31 +3,42 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
-  return <Wrapper>navbar component</Wrapper>;
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = useAuth0();
+  const isUser = isAuthenticated && user;
+  console.log(user)
+  return <Wrapper>
+    {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+    {isUser && user.name && <h4><strong>{user.name.toUpperCase()}</strong></h4>}
+    {isUser ?
+      <button onClick={() => logout({ returnTo: window.location.origin })}>logout</button>
+      :
+      <button onClick={loginWithRedirect}>login</button>
+    }
+  </Wrapper>;
 };
 
 const Wrapper = styled.nav`
-  padding: 1.5rem;
-  margin-bottom: 4rem;
-  background: var(--clr-white);
-  text-align: center;
-  display: grid;
-  grid-template-columns: auto auto 100px;
-  justify-content: center;
-  align-items: center;
-  gap: 1.5rem;
-  h4 {
-    margin-bottom: 0;
+    padding: 1.5rem;
+    margin-bottom: 4rem;
+    background: var(--clr-white);
+    text-align: center;
+    display: grid;
+    grid-template-columns: auto auto 100px;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5rem;
+    h4 {
+      margin - bottom: 0;
     font-weight: 400;
   }
-  img {
-    width: 35px !important;
+    img {
+      width: 35px !important;
     height: 35px;
     border-radius: 50%;
     object-fit: cover;
   }
-  button {
-    background: transparent;
+    button {
+      background: transparent;
     border: transparent;
     font-size: 1.2rem;
     text-transform: capitalize;
@@ -35,6 +46,6 @@ const Wrapper = styled.nav`
     color: var(--clr-grey-5);
     cursor: pointer;
   }
-`;
+    `;
 
 export default Navbar;
